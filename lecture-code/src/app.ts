20f73276a3cdd3d974f6516a601336c2d747396c
JunHyeongB <aowlr0513@gmail.com>
2022-01-30 19:04:47 +0900

    feat: ImageComponent 추가

--- lecture-code/src/app.ts
+++ lecture-code/src/app.ts
@@ -1,10 +1,17 @@
-import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent } from "./components/page/page.js";
 
 class App {
   private readonly page: PageComponent;
-  constructor(element: HTMLElement) {
+  constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
-    this.page.AttachTo(element);
+    this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      "Image Component",
+      "https://picsum.photos/600/300"
+    );
+    image.attachTo(appRoot, "beforeend");
   }
 }
 
