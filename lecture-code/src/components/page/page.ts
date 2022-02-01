e48dd5f59051007578ae09fdcda6d26b2cdc317b
JunHyeongB <aowlr0513@gmail.com>
2022-02-01 21:05:39 +0900

    refactor: 확장성 관련 Dependency Injection 리팩토링

--- lecture-code/src/components/page/page.ts
+++ lecture-code/src/components/page/page.ts
@@ -6,9 +6,17 @@ export interface Composable {
 
 type OnCloseListener = () => void;
 
-class PageItemComponent
+export interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+export type SectionContainerConstructor = {
+  new (): SectionContainer;
+};
+
+export class PageItemComponent
   extends BaseComponent<HTMLElement>
-  implements Composable
+  implements SectionContainer
 {
   private closeListener?: OnCloseListener;
 
@@ -34,7 +42,7 @@ class PageItemComponent
     child.attachTo(container);
   }
 
-  setCloseListener(listener: OnCloseListener) {
+  setOnCloseListener(listener: OnCloseListener) {
     this.closeListener = listener;
   }
 }
@@ -43,15 +51,15 @@ export class PageComponent
   extends BaseComponent<HTMLUListElement>
   implements Composable
 {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
   }
 
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
-    item.setCloseListener(() => {
+    item.setOnCloseListener(() => {
       item.removeFrom(this.element);
     });
   }
