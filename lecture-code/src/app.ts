ac2206e0c722181a1610fa354a43e9d302c078c1
JunHyeongB <aowlr0513@gmail.com>
2022-02-01 22:53:20 +0900

    feat: Drag & Drop 기본 이벤트 설정

--- lecture-code/src/app.ts
+++ lecture-code/src/app.ts
@@ -49,6 +49,27 @@ class App {
       TextSectionInput,
       (input: TextSectionInput) => new TodoComponent(input.title, input.body)
     );
+
+    this.page.addChild(
+      new ImageComponent("Image Title", "https://picsum.photos/800/400")
+    );
+    this.page.addChild(
+      new VideoComponent("Video Title", "https://youtu.be/D7cwvvA7CP0")
+    );
+    this.page.addChild(
+      new NoteComponent("Note Title", "Don't forget to code your dream")
+    );
+    this.page.addChild(new TodoComponent("Todo Title", "Typescript Course!"));
+    this.page.addChild(
+      new ImageComponent("Image Title", "https://picsum.photos/800/400")
+    );
+    this.page.addChild(
+      new VideoComponent("Video Title", "https://youtu.be/D7cwvvA7CP0")
+    );
+    this.page.addChild(
+      new NoteComponent("Note Title", "Don't forget to code your dream")
+    );
+    this.page.addChild(new TodoComponent("Todo Title", "Typescript Course!"));
   }
 
   private bindElementToDialog<T extends (MediaData | TextData) & Component>(
--- lecture-code/src/components/page/page.ts
+++ lecture-code/src/components/page/page.ts
@@ -22,7 +22,7 @@ export class PageItemComponent
 
   constructor() {
     super(`
-    <li class="page-item">
+    <li draggable="true" class="page-item">
       <section class="page-item__body"></section>
       <div class="page-item__controls">
         <button class="close">&times;</button>
@@ -33,6 +33,21 @@ export class PageItemComponent
     closeBtn.onclick = () => {
       this.closeListener && this.closeListener();
     };
+
+    this.element.addEventListener("dragstart", (event: DragEvent) => {
+      this.onDragStart(event);
+    });
+
+    this.element.addEventListener("dragend", (event: DragEvent) => {
+      this.onDragEnd(event);
+    });
+  }
+
+  onDragStart(event: DragEvent) {
+    console.log("dragstart", event);
+  }
+  onDragEnd(event: DragEvent) {
+    console.log("dragend", event);
   }
 
   addChild(child: Component) {
@@ -53,6 +68,23 @@ export class PageComponent
 {
   constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
+
+    this.element.addEventListener("dragover", (event: DragEvent) => {
+      this.onDragOver(event);
+    });
+
+    this.element.addEventListener("drop", (event: DragEvent) => {
+      this.onDragDrop(event);
+    });
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    console.log("dragover", event);
+  }
+  onDragDrop(event: DragEvent) {
+    event.preventDefault();
+    console.log("drop", event);
   }
 
   addChild(section: Component) {
